6568eaf77bb641ead1ff446917e0f26233fff61d
Matt Hayes <matt@mysterycommand.com>
2025-08-16 17:34:10 -0400

    refactor: new Response() --> fres()

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-Deno.serve(
-  () =>
-    new Response("501 - Not Implemented", {
-      status: 501,
-      statusText: "Not Implemented",
-    })
+import { fres } from "./server/fres.ts";
+
+declare global {
+  type F<T> = T extends new (...args: infer A) => infer R
+    ? (...args: A) => R
+    : never;
+}
+
+Deno.serve(() =>
+  fres("501 - Not Implemented", {
+    status: 501,
+    statusText: "Not Implemented",
+  })
 );
---
+++ server/fres.ts
@@ -0,0 +1,2 @@
+export const fres: F<typeof Response> = (body, init) =>
+  new Response(body, init);
