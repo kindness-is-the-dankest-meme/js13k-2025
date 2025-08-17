412c92f9c346a3f6b485e9fc8741d157edd71f0a
Matt Hayes <matt@mysterycommand.com>
2025-08-16 22:42:08 -0400

    feat: re-implement my dev-server set up for files and transforms, no .tsx support here

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+import { Exts } from "./server/exts.ts";
+import { fres } from "./server/fres.ts";
+import { furl } from "./server/furl.ts";
+import { mapf } from "./server/mapf.ts";
+import { nrml } from "./server/nrml.ts";
+import { open } from "./server/open.ts";
+import { mime } from "./server/mime.ts";
+import { prod } from "./server/prod.ts";
 import { stat } from "./server/stat.ts";
+import { tsfm } from "./server/tsfm.ts";
 import { wtch } from "./server/wtch.ts";
 
 declare global {
@@ -9,7 +18,34 @@ declare global {
 
 const fs = await wtch("public");
 
-Deno.serve(() => {
-  console.log(fs);
+Deno.serve(async ({ url }) => {
+  const { dir, name, ext } = mapf(url, fs),
+    path = nrml(dir, name, ext);
+
+  if (!dir.startsWith("/")) return stat(403);
+  if (!fs.has(path)) return stat(404);
+
+  try {
+    switch (ext) {
+      case Exts.Css:
+      case Exts.Html: {
+        return fres(await open(furl(path, Deno.mainModule)), {
+          headers: { "Content-Type": mime(ext) },
+        });
+      }
+
+      case Exts.Ts: {
+        return fres(await tsfm(furl(path, Deno.mainModule)), {
+          headers: { "Content-Type": mime(Exts.Js) },
+        });
+      }
+    }
+  } catch (e) {
+    const error = e instanceof Error ? e : new Error(String(e));
+    console.error(error);
+
+    return prod ? stat(500) : stat(404);
+  }
+
   return stat(501);
 });
