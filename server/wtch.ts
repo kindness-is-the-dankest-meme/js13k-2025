3b87f4e9cbe1c520cf511c6e338eed44137c1b35
Matt Hayes <matt@mysterycommand.com>
2025-08-16 22:09:02 -0400

    feat: a directory watcher that keeps a Set of pathnames up to date

---
+++ server/wtch.ts
@@ -0,0 +1,28 @@
+import { walk } from "./walk.ts";
+
+export const wtch = async (dirname: string) => {
+  const fs = new Set<string>(
+    await Array.fromAsync(walk(dirname, Deno.mainModule))
+  );
+
+  (async () => {
+    const dir = Deno.watchFs(dirname, { recursive: true });
+
+    for await (const { kind, paths } of dir) {
+      switch (kind) {
+        case "create":
+        case "rename": {
+          paths.forEach((p) => fs.add(p));
+          break;
+        }
+
+        case "remove": {
+          paths.forEach((p) => fs.delete(p));
+          break;
+        }
+      }
+    }
+  })();
+
+  return fs;
+};
