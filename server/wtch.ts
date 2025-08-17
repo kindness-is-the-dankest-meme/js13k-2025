c7e356f152fc1bc52ef1163c8577092e649cae2e
Matt Hayes <matt@mysterycommand.com>
2025-08-16 23:52:17 -0400

    refactor: make sure the paths we're matching in the set are relative to the watched directory

--- server/wtch.ts
+++ server/wtch.ts
@@ -1,26 +1,41 @@
+import { dirname } from "jsr:@std/path/dirname";
+import { relative } from "jsr:@std/path/relative";
 import { walk } from "./walk.ts";
+import { furl } from "./furl.ts";
 
-export const wtch = async (dirname: string) => {
+export const wtch = async (d: string) => {
+  /**
+   * is this roughly the same as `URL.fileURLToPath`? ... maybe this just won't
+   * work on Windows environments?
+   *
+   * @see https://docs.deno.com/api/node/url/~/fileURLToPath
+   */
+  const mdir = dirname(furl(Deno.mainModule).pathname);
+
   const fs = new Set<string>(
-    await Array.fromAsync(walk(dirname, Deno.mainModule))
+    /**
+     * n.b. `Deno.mainModule` here is
+     * `"file:///Users/matt/Sites/kitdm/js13k-2025/main.ts"`
+     */
+    await Array.fromAsync(walk(d, Deno.mainModule))
   );
 
   (async () => {
     /**
      * @see https://docs.deno.com/examples/watching_files/
      */
-    const dir = Deno.watchFs(dirname, { recursive: true });
+    const dir = Deno.watchFs(d, { recursive: true });
 
     for await (const { kind, paths } of dir) {
       switch (kind) {
         case "create":
         case "rename": {
-          paths.forEach((p) => fs.add(p));
+          paths.forEach((p) => fs.add(relative(mdir, p)));
           break;
         }
 
         case "remove": {
-          paths.forEach((p) => fs.delete(p));
+          paths.forEach((p) => fs.delete(relative(mdir, p)));
           break;
         }
       }
